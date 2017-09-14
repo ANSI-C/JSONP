a84a5255c3bc2a6be95bc0250b153aa3a0924804
Anselmo Canfora <strollicu@gmail.com>
2017-09-14 12:48:13 +0200

    versions 1.76

--- lib/JSONP.pm
+++ lib/JSONP.pm
@@ -12,7 +12,7 @@ use Digest::SHA;
 use JSON;
 use Want;
 
-our $VERSION = '1.75';
+our $VERSION = '1.76';
 
 =encoding utf8
 
@@ -748,9 +748,9 @@ sub serialize
 {
 	my ($self) = @_;
 	my $out;
-	my $pretty = reftype $self eq 'HASH' && defined $self->{_pretty} && $self->{_pretty} ? 1 : 0;
+	my $pretty = reftype $self eq 'HASH' && $self->{_pretty} ? 1 : 0;
 	eval{
-		$out = JSON->new->utf8->pretty($pretty)->allow_blessed->convert_blessed->encode($self);
+		$out = JSON->new->ascii->pretty($pretty)->allow_blessed->convert_blessed->encode($self);
 	} || $@;
 }
 
